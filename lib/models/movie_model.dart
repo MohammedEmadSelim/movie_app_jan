427a68f9ad48aa09890493f20ce43cc0f07877b3
caesar <abdulhamidiskander@gmail.com>
2026-02-17 06:26:33 +0200

    feat: implement `MovieModel` class
    - Create `MovieModel` class in `lib/models/movie_model.dart` with `id`, `title`, and `image` fields.
    - Define a constructor with required parameters for all fields.

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -8,6 +8,13 @@ MovieModel({
   required this.title,
   required this.image
   });
-
-
+  factory MovieModel.fromJson(Map<String, dynamic> json){
+    if (json != null){
+      return MovieModel(
+          id: json['id'],
+          title: json['title'],
+          image: json['poster_path'] ?? ''
+      );
+    }
+  }
 }
