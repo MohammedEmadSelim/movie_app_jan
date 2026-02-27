a4ec919265931c8f15bb17145f9ca98b55b0422b
caesar <abdulhamidiskander@gmail.com>
2026-02-27 02:53:29 +0200

    feat: implement debounce for movie search in `SearchScreen`
    - Import `dart:async` to use the `Timer` class.
    - Implement a 500ms debounce timer in the `TextField.onChanged` callback to limit API calls during typing.
    - Update `TextField` decoration with a new background color, increased border radius, and centered content padding.
    - Replace `suffixIcon` with `prefixIcon` for the search icon in the search bar.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import '../cubit/movies_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,26 +8,40 @@ import '../widgets/movie_item.dart';
 
 
 class SearchScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    Timer? timer;
     return Scaffold(
       backgroundColor: Color(0xFF1F242F),
       body: Padding(
         padding: EdgeInsets.all(15),
         child: Column(
           children: [
-            TextField(
-              onChanged: (value) => context.read<MoviesCubit>().search(value),
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFF2D3543),
-                hintText: "Spiderman",
-                hintStyle: TextStyle(color: Colors.grey),
-                suffixIcon: Icon(Icons.search, color: Colors.grey),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(15), borderSide: BorderSide.none),
-              ),
-            ),
+
+        TextField(
+        onChanged: (text) {
+      if (timer?.isActive ?? false) timer?.cancel();
+      timer = Timer(const Duration(milliseconds: 500), () {
+      if (text.isNotEmpty) {
+      context.read<MoviesCubit>().search(text);
+      }
+      });
+      },
+        style: const TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: const Color(0xFF242C3B),
+          hintText: "Spiderman",
+          hintStyle: const TextStyle(color: Colors.grey),
+          prefixIcon: const Icon(Icons.search, color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide.none,
+          ),
+          contentPadding: const EdgeInsets.symmetric(vertical: 15),
+        ),
+      )  ,
             Expanded(
               child: BlocBuilder<MoviesCubit, MoviesState>(
                 builder: (context, state) {
